c64bc3052d05ede81ede64165949fb31fbca71d0
phelisa <phelisajeruto@gmail.com>
2022-04-14 09:28:33 +0300

    assesment

---
+++ src/main.kt
@@ -0,0 +1,65 @@
+fun main(){
+    var Sang=Currentaccount("Phelisia","234567",10000.00)
+    Sang.deposit(5000.00)
+    println(Sang.accountBalance)
+    Sang.withdrawn(1000.00)
+    println( Sang.accountBalance)
+    Sang.details()
+    fruits("Banana")
+    var Jeruto=savingsAccount("PJ","12345",20000.00,4)
+
+
+}
+ open class Currentaccount(var accountName:String,var accountNumber:String,var accountBalance:Double){
+    fun deposit(amount:Double){
+        accountBalance+=amount
+        println(amount)
+
+
+    }
+    open fun withdrawn(amount: Double){
+        accountBalance-=amount
+        println(amount)
+    }
+    fun details(){
+        println("Account number $accountNumber with balance ${accountBalance} is operated by $accountName")
+    }
+
+}
+  open class savingsAccount(accountName: String,accountNumber: String,accountBalance: Double , var withdrawals:Int ):Currentaccount(accountName,accountNumber,accountBalance,) {
+     override fun withdrawn(amount: Double) {
+      if(withdrawals<=4){
+
+      }
+     }
+
+
+}
+//A product is represented by a data class with these attributes: name,
+//weight, price, category. Category can either be groceries, hygiene or
+//other. Write a function that takes in a product and uses a when
+//statement to assign each product to a list based on its category
+//(3 points)
+
+data class Product(var name :String,var weight:String,var price:Int,var category: String){
+    fun takecategory(cate:List<Product>){
+        var newlist= mutableListOf<String>()
+        cate.forEach { b->
+
+        }
+
+    }
+
+}
+
+//4.Write a function that given a string returns a string composed of only
+//the characters in even indices. For example given “banana” it will return
+fun fruits(fruit:String):String{
+    fruit.forEachIndexed { index, c ->
+        if (index%2==0){
+            println(c)
+
+        }
+    }
+    return fruit
+}
